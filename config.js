d0e7e763d876e25ea1a99c9c47a37e87378824d1
Praharsh Jain <praharsh.jain@grabtaxi.com>
2023-01-24 16:50:48 +0530

    read config from package.json

--- config.js
+++ config.js
@@ -1,8 +1,16 @@
+const fs = require('fs');
+let data = null;
+try {
+    data = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+} catch (err) {
+    console.error(err);
+}
 module.exports = {
-    appName: 'Electron PDF Viewer',
-    appVersion: '1.0',
-    copyrightInfo: 'Copyright © Praharsh Jain, 2023',
-    author: 'Praharsh Jain',
-    website: 'https://praharsh.tech',
+    appName: data.name,
+    appVersion: data.version,
+    appDescription: data.description,
+    copyrightInfo: data.copyrightInfo,
+    author: data.author,
+    website: data.website,
     iconPath: require('path').join(__dirname, 'icon.png'),
 }
