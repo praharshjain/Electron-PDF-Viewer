2406ff00fad4fc9c5b396da3b93f03e22eeba43b
Praharsh Jain <praharsh.jain@grabtaxi.com>
2023-02-07 16:54:55 +0530

    added dist build for mac

---
+++ app/config.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const data = require(path.join(__dirname, 'config.json'));
+module.exports = {
+    appName: data.name,
+    appVersion: data.version,
+    appDescription: data.description,
+    copyrightInfo: data.copyright,
+    author: data.author,
+    website: data.website,
+    iconPath: path.join(__dirname, 'icon.png'),
+}
